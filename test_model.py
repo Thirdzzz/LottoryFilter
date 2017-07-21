9d5da1968eab3528bf8408d7b6ad983dad30533d
laohanzzz <laohanzzz@gmail.com>
2017-07-21 11:01:06 +0800

    get_together

--- test_model.py
+++ test_model.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!usr/bin/env python  
 #-*- coding: utf-8 -*-  
   
@@ -28,35 +27,4 @@ def predict(model, test_x, test_y, is_binary_class):
     return predict
     
 
-=======
-#!usr/bin/env python  
-#-*- coding: utf-8 -*-  
-  
-import sys  
-import os  
-import time  
-from sklearn import metrics
-import numpy as np  
-import pickle 
-import data
-  
-reload(sys)  
-sys.setdefaultencoding('utf8')  
-  
-def predict(model, test_x, test_y, is_binary_class):
-    predict = model.predict(test_x)  
-    F1Score = 0
-    #if model_save_file != None:  
-    #    model_save[classifier] = model  
-    if is_binary_class:  
-        precision = metrics.precision_score(test_y, predict)  
-        recall = metrics.recall_score(test_y, predict)  
-        F1Score = 2 * precision * recall / (precision + recall)
-        print 'precision: %.2f%%, recall: %.2f%%, F1Score: %.2f%%' % (100 * precision, 100 * recall, 100 * F1Score)  
-    accuracy = metrics.accuracy_score(test_y, predict)  
-    print 'accuracy: %.2f%%' % (100 * accuracy)  
-    return predict
-    
-
->>>>>>> 3db818bbf24d75180002c8646dd428fdc21faa24
   
